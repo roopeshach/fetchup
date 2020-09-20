8cbfe4b588ec27b72e66ede28056971fba1cfec3
ubuntu <rupeshacharya2014@gmail.com>
2020-09-20 20:25:20 +0545

    PHP response added and displayed in Front

--- index.js
+++ index.js
@@ -3,19 +3,42 @@ function fetchUp( options){
    
     const {url, headers,  max_size } = options
   
-    var inpFile = document.getElementById("fetchup")
+    var file = document.getElementById("fetchup")
  
         myForm.addEventListener('submit',e =>{
             e.preventDefault();
             var url = 'upload.php';
             var formData = new FormData()
  
-            formData.append("fetchup",inpFile.files[0])
+            formData.append("fetchup",file.files[0])
  
             fetch(url, {
                 method:"POST",
                 body:formData
             })
+            .then(
+                function(response) {
+                  if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' +
+                      response.status);
+                    return;
+                  }
+            
+                  // Examine the text in the response
+                  response.json().then(function(data) {
+                    alert(data.response)
+                    file.value = file.defaultValue;
+                  });
+                }
+              )
+              .catch(function(err) {
+                console.log('Fetch Error :-S', err);
+              });
+            
+            
+              
+        
+              
         });
   }
   
--- upload.php
+++ upload.php
@@ -1,5 +1,9 @@
 <?php
- 
+
 $target_dir = "files/" . basename($_FILES['fetchup']['name']);
-move_uploaded_file($_FILES['fetchup']['tmp_name'],$target_dir);
- 
+if(move_uploaded_file($_FILES['fetchup']['tmp_name'],$target_dir)){
+    $res->response = "File ".$_FILES['fetchup']['name']." is successfully uploaded";
+}else{
+    $res->response = "Something got an error ! File Upload failed";
+}
+echo json_encode($res);
